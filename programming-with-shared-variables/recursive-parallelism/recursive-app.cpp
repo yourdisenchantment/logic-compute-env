483abb4bdbe9bedb2747bb9ee0ba588c82f5a40c
yourdisenchantment <p.idisenchantment@gmail.com>
2025-10-01 00:05:50 +0300

    Add `recursive-app.cpp`

--- programming-with-shared-variables/recursive-parallelism/recursive-app.cpp
+++ programming-with-shared-variables/recursive-parallelism/recursive-app.cpp
@@ -1,8 +1,51 @@
 // ./programming-with-shared-variables/recursive-parallelism/recursive-app.cpp
 
+#include <cmath>
 #include <iostream>
 
 using namespace std;
 
+constexpr double R = 1.0;
+constexpr double eps = 0.001;
+
+double f(double x);
+double integrated(double a, double b, double fa, double fb, double s);
+
+double f(const double x) {
+    return sqrt(R * R - x * x);
+}
+
+double integrated(const double a, const double b, const double fa, const double fb, const double s) {
+    const double m = (a + b) / 2;
+    const double fm = f(m);
+
+    // const double sl = (b - a) * (fa + fm) / 4;
+    // const double sr = (b - a) * (fm + fb) / 4;
+
+    const double sl = (m - a) * (fa + fm) / 2;
+    const double sr = (b - m) * (fm + fb) / 2;
+
+    if (abs(sl + sr - s) < eps * 3) {
+        return sl + sr;
+    }
+
+    return integrated(a, m, fa, fm, sl) + integrated(m, b, fm, fb, sr);
+}
+
 int main() {
+    constexpr double a = 0.0;
+    constexpr double b = R;
+
+    const double fa = f(a);
+    const double fb = f(b);
+
+    const double s = (b - a) * (fa + fb) / 2;
+
+    const double result = integrated(a, b, fa, fb, s);
+    constexpr double area = M_PI * R * R / 4;
+    const double error = abs(result - area);
+
+    cout << result << endl;
+    cout << area << endl;
+    cout << error << endl;
 }
