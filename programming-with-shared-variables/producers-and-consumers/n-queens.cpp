41267aceef1b0ef4592c22c85ba3ca01d451f88b
yourdisenchantment <p.idisenchantment@gmail.com>
2025-10-07 15:58:28 +0300

    Add new tasks and rename bin files

---
+++ programming-with-shared-variables/producers-and-consumers/n-queens.cpp
@@ -0,0 +1,7 @@
+// ./programming-with-shared-variables/producers-and-consumers/n-qweens.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {}
---
+++ programming-with-shared-variables/producers-and-consumers/shared-variables.cpp
@@ -0,0 +1,7 @@
+// ./programming-with-shared-variables/producers-and-consumers/shared-variables.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {}
