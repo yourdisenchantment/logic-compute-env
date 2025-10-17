0093ed60236edcf89461937a1f44c3897e454783
yourdisenchantment <p.idisenchantment@gmail.com>
2025-10-18 02:43:31 +0300

    Reformat code

--- programming-with-shared-variables/producers-and-consumers/attachments/stones.cpp
+++ programming-with-shared-variables/producers-and-consumers/attachments/stones.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
 int n;
@@ -7,34 +7,31 @@ int w[20];
 
 int side[20];
 
-int solve(int N, int w_left, int w_right)
-{
-	if ( N == 0 )
-	{
-		for(int i=0;i<n;i++)
-			cout << side[i];
-		cout << ":" << abs ( w_left- w_right ) << endl;
-		return abs ( w_left- w_right );
-	}
-	
-	int res;
-	
-	side[N-1] = 1;
-	res = solve ( N-1, w_left + w[N-1], w_right );
-	
-	side[N-1] = 2;
-	res = min(res, solve ( N-1, w_left, w_right + w[N-1] ) );
-	
-	return res;
+int solve(int N, int w_left, int w_right) {
+    if (N == 0) {
+        for (int i = 0; i < n; i++)
+            cout << side[i];
+        cout << ":" << abs(w_left - w_right) << endl;
+        return abs(w_left - w_right);
+    }
+
+    int res;
+
+    side[N - 1] = 1;
+    res = solve(N - 1, w_left + w[N - 1], w_right);
+
+    side[N - 1] = 2;
+    res = min(res, solve(N - 1, w_left, w_right + w[N - 1]));
+
+    return res;
 }
 
-int main()
-{
-	cin >> n;
-	for(int i=0;i<n;i++)
-		cin >> w[i];
-		
-	cout << solve( n, 0, 0 ) << endl;
-	
-	return 0;
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> w[i];
+
+    cout << solve(n, 0, 0) << endl;
+
+    return 0;
 }
--- programming-with-shared-variables/producers-and-consumers/attachments/thread_pool.cpp
+++ programming-with-shared-variables/producers-and-consumers/attachments/thread_pool.cpp
@@ -1,70 +1,63 @@
 
-#include <iostream>
-#include <condition_variable>
-#include <thread>
-#include <chrono>
-#include <queue>
-#include <mutex>
-#include <math.h>
 #include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <iostream>
+#include <math.h>
+#include <mutex>
+#include <queue>
+#include <thread>
 using namespace std;
 
-class Task
-{
+class Task {
 public:
     double l, r, eps;
     void solve();
 };
 
-class QueueClosedException
-{
-};
+class QueueClosedException {};
 
-class BlockingQueue
-{
-   bool closed;
-   queue<Task> q;
-   mutex mx;
-   condition_variable cv_empty;
+class BlockingQueue {
+    bool closed;
+    queue<Task> q;
+    mutex mx;
+    condition_variable cv_empty;
+
 public:
-   BlockingQueue() {
-	   closed = false;
-   }
+    BlockingQueue() {
+        closed = false;
+    }
 
-   void close()
-   {
-	   closed = true;
+    void close() {
+        closed = true;
 
-	   cv_empty.notify_all();
-   }
+        cv_empty.notify_all();
+    }
 
-   void push(Task task)
-   {
-	   unique_lock lk(mx);
-	   q.push(task);
+    void push(Task task) {
+        unique_lock lk(mx);
+        q.push(task);
 
-	   cv_empty.notify_one();
-   }
+        cv_empty.notify_one();
+    }
 
-   Task get()
-   {
-	   unique_lock lk(mx);
+    Task get() {
+        unique_lock lk(mx);
 
-	   // wait for !q.empty()
-	   this->cv_empty.wait( lk, [this]{ return !q.empty() || closed; } );
+        // wait for !q.empty()
+        this->cv_empty.wait(lk, [this] { return !q.empty() || closed; });
 
-	   if(closed)
-		   throw QueueClosedException();
+        if (closed)
+            throw QueueClosedException();
 
-	   Task t = q.front();
-	   q.pop();
+        Task t = q.front();
+        q.pop();
 
-	   return t;
-   }
+        return t;
+    }
 };
 
-class ThreadPool
-{
+class ThreadPool {
     int threadCount;
     BlockingQueue tasks;
     vector<thread> threads;
@@ -77,43 +70,34 @@ public:
     void close();
 };
 
-void threadPoolFunction(ThreadPool* pool)
-{
-	try {
-		while(true)
-		{
-			Task task = pool->getTask();
-			task.solve();
-		}
-	} catch(QueueClosedException &e)
-	{
-	}
+void threadPoolFunction(ThreadPool* pool) {
+    try {
+        while (true) {
+            Task task = pool->getTask();
+            task.solve();
+        }
+    } catch (QueueClosedException& e) {
+    }
 }
 
-ThreadPool::ThreadPool(int threadCount):
-		threadCount(threadCount),
-		threads(threadCount)
-{
-	for(int i=0;i < this->threadCount ;i++)
-		this->threads[i] = thread(threadPoolFunction, this);
+ThreadPool::ThreadPool(int threadCount) : threadCount(threadCount), threads(threadCount) {
+    for (int i = 0; i < this->threadCount; i++)
+        this->threads[i] = thread(threadPoolFunction, this);
 }
 
-void ThreadPool::addTask(Task task)
-{
-	tasks.push(task);
+void ThreadPool::addTask(Task task) {
+    tasks.push(task);
 }
 
-Task ThreadPool::getTask()
-{
-	return tasks.get();
+Task ThreadPool::getTask() {
+    return tasks.get();
 }
 
-void ThreadPool::close()
-{
-	tasks.close();
+void ThreadPool::close() {
+    tasks.close();
 
-	for(int i=0;i < this->threadCount ;i++)
-		this->threads[i].join();
+    for (int i = 0; i < this->threadCount; i++)
+        this->threads[i].join();
 }
 
 ThreadPool pool(8);
@@ -125,69 +109,62 @@ condition_variable wait_parts;
 mutex parts_mutex;
 int parts;
 
-double f(double x)
-{
-	return sin(x);
+double f(double x) {
+    return sin(x);
 }
 
-void Task::solve()
-{
-	double area = ( f(this->l) + f(this->r))/2.0 * ( this->r - this->l );
+void Task::solve() {
+    double area = (f(this->l) + f(this->r)) / 2.0 * (this->r - this->l);
 
-	double m = (this->l + this->r)/2;
-	double area2 = ( ( f( this->l ) + 2 * f( m )+ f ( this->r ) ) * (this->r - this->l)/4.0 );
+    double m = (this->l + this->r) / 2;
+    double area2 = ((f(this->l) + 2 * f(m) + f(this->r)) * (this->r - this->l) / 4.0);
 
-	if ( fabs(area - area2) > this->eps )
-	{
-		Task t1, t2;
-		t1.l = this->l;
-		t1.r = m;
-		t1.eps = this->eps;
+    if (fabs(area - area2) > this->eps) {
+        Task t1, t2;
+        t1.l = this->l;
+        t1.r = m;
+        t1.eps = this->eps;
 
-		t2.l = m;
-		t2.r = this->r;
-		t2.eps = this->eps;
+        t2.l = m;
+        t2.r = this->r;
+        t2.eps = this->eps;
 
-		{
-			unique_lock lk(parts_mutex);
-			parts+=2;
-		}
+        {
+            unique_lock lk(parts_mutex);
+            parts += 2;
+        }
 
-		pool.addTask(t1);
-		pool.addTask(t2);
-	}
-	else
-	{
-		{
-			unique_lock lk(answer_mutex);
-			answer += area2;
-		}
+        pool.addTask(t1);
+        pool.addTask(t2);
+    } else {
+        {
+            unique_lock lk(answer_mutex);
+            answer += area2;
+        }
+    }
 
-	}
-
-	{
-		unique_lock lk(parts_mutex);
-		parts-=1;
-		wait_parts.notify_one();
-	}
+    {
+        unique_lock lk(parts_mutex);
+        parts -= 1;
+        wait_parts.notify_one();
+    }
 }
 
-int main()
-{
-	Task start;
-	start.l = 0;
-	start.r = 1;
-	start.eps = 1e-7;
-	parts = 1;
+int main() {
+    Task start;
+    start.l = 0;
+    start.r = 1;
+    start.eps = 1e-7;
+    parts = 1;
 
-	pool.addTask(start);
+    pool.addTask(start);
 
-	{
-		unique_lock lk(parts_mutex);
-		wait_parts.wait(lk, [] { return parts == 0; } );
-	}
+    {
+        unique_lock lk(parts_mutex);
+        wait_parts.wait(lk, [] { return parts == 0; });
+    }
 
-	printf("%.10lf\n", answer);
+    printf("%.10lf\n", answer);
 
-	pool.close();
+    pool.close();
 }
